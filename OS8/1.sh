146af2544f22f608dbc395efe246e69872ea1895
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-12-24 17:39:42 +0330

    Update 1.sh

--- OS8/1.sh
+++ OS8/1.sh
@@ -1,3 +1,3 @@
 sudo apt install git
 git config --global user.name "RezaMokaram"
-git config --global user.email "rezamokaramdost@gmail.com"
+git config --global user.email rezamokaramdost@gmail.com
