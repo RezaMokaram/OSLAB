dfc87fb70b0fbe5e5488df18dbc914bb4c7e355a
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2021-01-14 03:15:55 +0330

    Create 1.sh

---
+++ OS11/1.sh
@@ -0,0 +1,22 @@
+echo "first number X second number"
+echo "Please Insert Your Operator:"
+read sine
+echo "Please insert first number"
+read num1
+echo "Please insert second one"
+read num2
+if [ $sine = "+" ]
+then
+    echo $(($num1 + $num2))
+elif [ $sine = "-" ]
+then
+    echo $(($num1 - $num2))
+elif [ $sine = "x" ]
+then
+    echo $(($num1 * $num2))
+elif [ $sine = "/" ]
+then
+    echo $(($num1 / $num2))
+else
+    echo "Invalid!!!!!!!!!!!!!!"
+fi
