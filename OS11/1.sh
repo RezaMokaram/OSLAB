d65f5ad04caf782de29e7e9255b134a1ab33348b
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2021-01-14 03:47:38 +0330

    Update 1.sh

--- OS11/1.sh
+++ OS11/1.sh
@@ -1,22 +1,22 @@
-echo "first number X second number"
+echo "first variable X second variable"
 echo "Please Insert Your Operator(+,-,*,/):"
 read sine
-echo "Please insert first number"
-read num1
+echo "Please insert first variable"
+read variable1
 echo "Please insert second one"
-read num2
+read variable2
 if [ $sine = "+" ]
 then
-    echo $(($num1 + $num2))
+    echo $(($variable1 + $variable2))
 elif [ $sine = "-" ]
 then
-    echo $(($num1 - $num2))
+    echo $(($variable - $variable2))
 elif [ $sine = "*" ]
 then
-    echo $(($num1 * $num2))
+    echo $(($variable1 * $variable2))
 elif [ $sine = "/" ]
 then
-    echo $(($num1 / $num2))
+    echo $(($variable1 / $variable2))
 else
     echo "Invalid!!!!!!!!!!!!!!"
 fi
